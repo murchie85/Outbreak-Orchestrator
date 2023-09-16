f8f9d9019592890eebe3b7f306870f102f3a7a44
murchie85 <murchie85@gmail.com>
2023-09-16 13:00:05 +0100

    basics

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const organicMatterEl = document.getElementById("organicMatter");
     const rodentCountEl = document.getElementById("rodentCount");
-    const collectBtn = document.getElementById("collectBtn");
+    const collectBtn = document.getElementById("collectBtn");  // using 'collectBtn' ID
 
     // Load saved data from localStorage or use default values
     const organicMatter = localStorage.getItem("organicMatter") || 2123000;
@@ -9,6 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     organicMatterEl.innerText = organicMatter;
     rodentCountEl.innerText = rodentCount;
+
+
+
+
+    // ----------COLLECT BUTTON 
 
     collectBtn.addEventListener("click", function () {
         let organicMatterValue = parseInt(organicMatterEl.innerText);
@@ -23,5 +28,21 @@ document.addEventListener("DOMContentLoaded", function () {
         // Save to localStorage
         localStorage.setItem("organicMatter", organicMatterValue);
         localStorage.setItem("rodentCount", rodentCountValue);
+
+        // Border color change logic
+        this.classList.add("pressed");
+
+        // After 1/6th of a second (166.7ms), remove the pressed class
+        setTimeout(() => {
+            this.classList.remove("pressed");
+        }, 200);
     });
+
+
+
+
+
+
+
+
 });
